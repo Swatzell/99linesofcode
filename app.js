88d1e96063a9926417f2eda0d4abb79c3ddf4404
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-08-28 19:28:47 -0500

    added a loop statement to countdown from 99

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let friends = ["adam","scotty","blake","kyle","gibbs"];
-let i = 99
+const friends = ["adam", "scotty", "blake", "kyle", "gibbs"];
+
+for (let i = 0; i < 5; i++) {
+    console.log(friends[i].toUpperCase() + ":");
+
+    for (let j = 99; j > 0; j--) {
+        console.log([j] + " " + "Lines of code in the file");
+    }
+
+
+}
+
+
+
