d3f93302c6854aeac274e0327d734e3c9c6235f1
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-10 17:51:26 -0500

    removed unnecesary quotations used for spacing in the if and else if statements

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ for (let i = 0; i < 5; i++) {
 
     for (let j = 5; j > 0; j--) {
         if (j === 1) {
-            console.log(j + " " + "Line of code in the file," + j + " Line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+            console.log(j + " Line of code in the file," + j + " Line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
         } else if (j === 2) {
-            console.log(j + " " + "Lines of code in the file," + j + " Lines of code; " + friends[i] + " strikes one out, clears it all out," + (j - 1) + " Line of code in the file")
+            console.log(j + " Lines of code in the file," + j + " Lines of code; " + friends[i] + " strikes one out, clears it all out," + (j - 1) + " Line of code in the file")
         } else {
-            console.log(j + " " + "Lines of code in the file," + j + " Lines of code; " + friends[i] + " strikes one out, clears it all out," + (j - 1) + " Lines of code in the file");
+            console.log(j + " Lines of code in the file," + j + " Lines of code; " + friends[i] + " strikes one out, clears it all out," + (j - 1) + " Lines of code in the file");
 
         }
     }
